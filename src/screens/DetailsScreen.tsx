f11228808f440395407ecf2a4358ce488d73def5
Evgxn <jeka2285311@mail.ru>
2023-12-08 15:43:23 +0300

    add coffee size in detailsScreen

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,18 +1,35 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet, StatusBar, ScrollView} from 'react-native';
-import {useStore} from '../store/store';
-import {COLORS} from '../theme/theme';
-import ImageBackgroundInfo from '../components/ImageBackgroundInfo';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  ScrollView,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+} from "react-native";
+import { useStore } from "../store/store";
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from "../theme/theme";
+import ImageBackgroundInfo from "../components/ImageBackgroundInfo";
 
-const DetailsScreen = ({navigation, route}: any) => {
+const DetailsScreen = ({ navigation, route }: any) => {
   const ItemOfIndex = useStore((state: any) =>
-    route.params.type == 'Coffee' ? state.CoffeeList : state.BeanList,
+    route.params.type == "Coffee" ? state.CoffeeList : state.BeanList
   )[route.params.index];
 
   const addToFavoriteList = useStore((state: any) => state.addToFavoriteList);
   const deleteFromFavoriteList = useStore(
-    (state: any) => state.deleteFromFavoriteList,
+    (state: any) => state.deleteFromFavoriteList
   );
+
+  const [price, setPrice] = useState(ItemOfIndex.prices[0]);
+  const [fullDescription, setFullDescription] = useState(false);
 
   const ToggleFavourite = (favourite: boolean, type: string, id: string) => {
     favourite ? deleteFromFavoriteList(type, id) : addToFavoriteList(type, id);
@@ -27,7 +44,8 @@ const DetailsScreen = ({navigation, route}: any) => {
       <StatusBar backgroundColor={COLORS.primaryBlackHex} />
       <ScrollView
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={styles.ScrollViewFlex}>
+        contentContainerStyle={styles.ScrollViewFlex}
+      >
         <ImageBackgroundInfo
           EnableBackHandler={true}
           imagelink_portrait={ItemOfIndex.imagelink_portrait}
@@ -43,6 +61,67 @@ const DetailsScreen = ({navigation, route}: any) => {
           BackHandler={BackHandler}
           ToggleFavourite={ToggleFavourite}
         />
+        <View style={styles.FooterInfoArea}>
+          <Text style={styles.InfoTitle}>Description</Text>
+          {fullDescription ? (
+            <TouchableWithoutFeedback
+              onPress={() => {
+                setFullDescription((prev) => !prev);
+              }}
+            >
+              <Text style={styles.DescText}>{ItemOfIndex.description}</Text>
+            </TouchableWithoutFeedback>
+          ) : (
+            <TouchableWithoutFeedback
+              onPress={() => {
+                setFullDescription((prev) => !prev);
+              }}
+            >
+              <Text numberOfLines={3} style={styles.DescText}>
+                {ItemOfIndex.description}
+              </Text>
+            </TouchableWithoutFeedback>
+          )}
+          <Text style={styles.InfoTitle}>Size</Text>
+          <View style={styles.SizeOuterContainer}>
+            {ItemOfIndex.prices.map((data: any) => (
+              <TouchableOpacity
+                onPress={() => {
+                  setPrice(data);
+                }}
+                key={data.size}
+                style={[
+                  styles.SizeBox,
+                  {
+                    borderColor:
+                      data.size == price.size
+                        ? COLORS.primaryOrangeHex
+                        : COLORS.primaryDarkGreyHex,
+                  },
+                ]}
+              >
+                <Text
+                  style={[
+                    styles.SizeText,
+                    {
+                      fontSize:
+                        ItemOfIndex.type == "bean"
+                          ? FONTSIZE.size_14
+                          : FONTSIZE.size_16,
+                      color:
+                        data.size == price.size
+                          ? COLORS.primaryOrangeHex
+                          : COLORS.secondaryLightGreyHex,
+                    },
+                  ]}
+                >
+                  {data.size}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </View>
+        
       </ScrollView>
     </View>
   );
@@ -55,6 +134,40 @@ const styles = StyleSheet.create({
   },
   ScrollViewFlex: {
     flexGrow: 1,
+  },
+  FooterInfoArea: {
+    padding: SPACING.space_20,
+  },
+  InfoTitle: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_16,
+    color: COLORS.primaryWhiteHex,
+    marginBottom: SPACING.space_10,
+  },
+  DescText: {
+    letterSpacing: 0.5,
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.primaryWhiteHex,
+    marginBottom: SPACING.space_30,
+  },
+  SizeOuterContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: SPACING.space_20,
+  },
+  SizeBox: {
+    flex: 1,
+    backgroundColor: COLORS.primaryDarkGreyHex,
+    alignItems: "center",
+    justifyContent: "center",
+    height: SPACING.space_24 * 2,
+    borderRadius: BORDERRADIUS.radius_10,
+    borderWidth: 2,
+  },
+  SizeText: {
+    fontFamily: FONTFAMILY.poppins_medium,
   },
 });
 
