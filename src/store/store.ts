d0d2509ce4116797001f88587ea562bb44e0c32d
Evgxn <jeka2285311@mail.ru>
2023-12-12 17:41:54 +0300

    add logic to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -104,6 +104,92 @@ export const useStore = create(
             console.log(state.FavoritesList.length);
           })
         ),
+      // incrementCartItemQuantity: (id: string, size: string) =>
+      //   set(
+      //     produce((state) => {
+      //       state.CartList.find((cartItem: any, index: number) => {
+      //         if (cartItem.id == id) {
+      //           cartItem.prices.find((priceItem: any) => {
+      //             if (priceItem.size == size) {
+      //               priceItem.quantity++;
+      //             }
+      //           });
+      //         }
+      //       });
+      //     })
+      //   ),
+      incrementCartItemQuantity: (id: string, size: string) =>
+        set(
+          produce((state) => {
+            for (let i = 0; i < state.CartList.length; i++) {
+              if (state.CartList[i].id == id) {
+                for (let j = 0; j < state.CartList[i].prices.length; j++) {
+                  if (state.CartList[i].prices[j].size == size) {
+                    state.CartList[i].prices[j].quantity++;
+                    break;
+                  }
+                }
+              }
+            }
+          })
+        ),
+      decrementCartItemQuantity: (id: string, size: string) =>
+        set(
+          produce((state) => {
+            for (let i = 0; i < state.CartList.length; i++) {
+              if (state.CartList[i].id == id) {
+                for (let j = 0; j < state.CartList[i].prices.length; j++) {
+                  if (state.CartList[i].prices[j].size == size) {
+                    if (state.CartList[i].prices.length > 1) {
+                      if (state.CartList[i].prices[j].quantity > 1) {
+                        state.CartList[i].prices[j].quantity--;
+                      } else {
+                        state.CartList[i].prices.splice(j, 1);
+                      }
+                    } else {
+                      if (state.CartList[i].prices[j].quantity > 1) {
+                        state.CartList[i].prices[j].quantity--;
+                      } else {
+                        state.CartList.splice(i, 1);
+                      }
+                    }
+                    break;
+                  }
+                }
+              }
+            }
+          })
+        ),
+      addToOrderHistoryListFromCart: () =>
+        set(
+          produce((state) => {
+            let temp = state.CartList.reduce(
+              (accumulator: number, currentValue: any) =>
+                accumulator + parseFloat(currentValue.ItemPrice),
+              0
+            );
+            if (state.OrderHistoryList.length > 0) {
+              state.OrderHistoryList.unshift({
+                OrderDate:
+                  new Date().toDateString() +
+                  " " +
+                  new Date().toLocaleTimeString(),
+                CartList: state.CartList,
+                CartListPrice: temp.toFixed(2).toString(),
+              });
+            } else {
+              state.OrderHistoryList.push({
+                OrderDate:
+                  new Date().toDateString() +
+                  " " +
+                  new Date().toLocaleTimeString(),
+                CartList: state.CartList,
+                CartListPrice: temp.toFixed(2).toString(),
+              });
+            }
+            state.CartList = [];
+          })
+        ),
     }),
     {
       name: "coffee-app",
